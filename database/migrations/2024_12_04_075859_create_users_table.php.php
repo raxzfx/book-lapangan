d270f4dfbc4c21eff2f9130d55d3bafbe4c3b486
rasya nazra <rasyanazraditya@gmail.com>
2024-12-04 15:29:55 +0700

    update

--- database/migrations/2024_12_04_075859_create_users_table.php.php
+++ database/migrations/2024_12_04_075859_create_users_table.php.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name',100);
             $table->string('email',100)->nullable();
             $table->string('password');
-            $table->enum('role',['admin','user'])->default('user');
+            $table->enum('role',['admin','user','ownerLapang'])->default('user');
             $table->integer('no_telp');
             $table->timestamps();
         });
